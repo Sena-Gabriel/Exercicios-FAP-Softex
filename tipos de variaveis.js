9f2faafbc85cce8b627b052cb7a3b30076edf3f3
Gabriel Sena <125525224+Sena-Gabriel@users.noreply.github.com>
2023-09-01 03:47:19 -0300

    Update and rename tipos de variaveis js to tipos de variaveis.js

--- tipos de variaveis.js
+++ tipos de variaveis.js
@@ -1,7 +1,8 @@
 https://blockly-demo.appspot.com/static/demos/code/index.html#8e7ter
 
-nome = 'João';
-idade = 20;
-altura = 1.7;
-maior_de_idade = true;
+var nome = 'João';
+var idade = 20;
+var altura = 1.7;
+var maior_de_idade = true;
+
 window.alert(['Nome: ',nome,' | Idade: ',idade,' | Altura: ',altura,' | É maior de idade? ',maior_de_idade].join(''));
